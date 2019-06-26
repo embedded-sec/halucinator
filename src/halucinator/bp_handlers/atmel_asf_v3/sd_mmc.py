ccaa908c3d0e90fc61f9418e58c57af042bde521
Abe Clements <aacleme@sandia.gov>
2019-06-26 14:13:53 -0400

    Cleaned up copyright info

--- src/halucinator/bp_handlers/atmel_asf_v3/sd_mmc.py
+++ src/halucinator/bp_handlers/atmel_asf_v3/sd_mmc.py
@@ -1,8 +1,6 @@
-# Copyright 2018 National Technology & Engineering Solutions of Sandia, LLC 
-# (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, there is a 
-# non-exclusive license for use of this work by or on behalf of the U.S. 
-# Government. Export of this data may require a license from the United States 
-# Government.
+# Copyright 2018 National Technology & Engineering Solutions of Sandia, LLC
+# (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S. 
+# Government retains certain rights in this software.
 
 
 from ...peripheral_models.sd_card import SDCardModel
